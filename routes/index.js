942956fccd35356f7cf6fade5dd7b3e0e0efc066
Samantha <samantha.goldstein31@gmail.com>
2015-06-17 19:49:15 -0400

    Modified css and added completion page

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ router.get('/', function(req, res, next) {
 router.get('/save', function(req, res){
   var idnum = model.save(req.query.msg);
 
-  res.redirect('/?success=true&id='+idnum);
+  // res.redirect('/?success=true&id='+idnum);
+  res.render('create', {id: idnum});
 });
 
 //get everything after colon
