3f8b5f9950d1df32b0f74c5e1585257e8f9a289c
Samantha <samantha.goldstein31@gmail.com>
2015-06-22 18:08:43 -0400

    Migrating stuff to mongodb

--- model/index.js
+++ model/index.js
@@ -1,48 +1,93 @@
-var uuid = require('uuid');
-var db = {};
-
-
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:45000/particles');
+var Schema = mongoose.Schema;
+var db = mongoose.connection;
+var particlesSchema = new Schema({
+  _id: String,
+  msg: String,
+  openedAt: Date
+});
+var Particles = mongoose.model('Particles', particlesSchema);
+var util = require('../util');
 
-// var Schema = mongoose.Schema,
-//   ObjectId = Schema.ObjectId;
+mongoose.connect('mongodb://localhost:45000/particlesdb');
 
-// var particlesDB = new Schema({
-//   msg: String,
-//   openedAt: Date
-// });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  // yay!
+});
+
+// var uuid = require('uuid');
+// var db = {};
+
+
+function getNextId(cb) {
+  return Particles.count(cb);
+}
 
 exports.save = function(message){
-  var idnum = uuid.v4();
-  db[idnum] = {
-    msg: message,
-    openedAt: 0
-  };
-  console.log(db);
-  return idnum;
+  getNextId(function(err, count) {
+    var particle = new Particle({
+      _id: count,
+      msg: message,
+      date: new Date
+    });
+    particle.save(function(err, particle) {
+      if (err) throw err;
+      console.log('Saved particle', particle);
+    });
+  });
 }
 
-exports.getById = function(id){
-  if (id in db) return db[id];
-  else throw new Error("Entry Not Found");
+exports.getById = function(id, cb) {
+  Particles.find({_id: id}, cb);
 }
 
-exports.setOpenedAt = function(id, date){
-  if (!db[id].openedAt){
-    db[id].openedAt = date;
-  }
-  console.log(db);
+// exports.getById = function(id){
+//   if (id in db) return db[id];
+//   else throw new Error("Entry Not Found");
+// }
+//
+
+exports.setOpenedAt = function(id, date, cb) {
+  getById(id, function(err, particle) {
+    if (err) cb(err);
+    if (!particle.openedAt) {
+      particle.openedAt = +new Date;
+      return particle.save(cb);
+    }
+    return cb(null, particle);
+  });
 }
 
+// exports.setOpenedAt = function(id, date){
+//   if (!db[id].openedAt){
+//     db[id].openedAt = date;
+//   }
+//   console.log(db);
+// }
 
-exports.getMessage = function(id){
-  if (id in db) return db[id].msg;
-  else throw new Error("Entry Not Found");
+
+exports.getMessage = function(id, cb) {
+  getById(id, function(err, particle) {
+    if (err) cb(err);
+    cb(null, particle.msg);
+  });
 }
+
+// exports.getMessage = function(id){
+//   if (id in db) return db[id].msg;
+//   else throw new Error("Entry Not Found");
+// }
 
 
 //MIGHT NOT NEED
-exports.getOpenedAt= function(id){
-  return db[id].openedAt;
+exports.getOpenedAt = function(id, cb) {
+  getById(id, function(err, particle) {
+    if (err) cb(err);
+    cb(null, particle.openedAt);
+  });
 }
+
+// exports.getOpenedAt= function(id){
+//   return db[id].openedAt;
+// }
