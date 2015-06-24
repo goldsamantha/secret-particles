56c11f4e4a0ef0426e8c8aa06cc26e42d9b07ef8
Samantha <samantha.goldstein31@gmail.com>
2015-06-24 16:08:10 -0400

    Particle play

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,16 @@ router.get('/', function(req, res, next) {
 router.get('/save', function(req, res, next){
   var idnum = model.save(req.query.msg, function(err, particle) {
     if (err) return next(err);
-    res.render('create', {id: particle._id});
+    // res.render('create', {id: particle._id});
+
+    res.render('create', {data: particle});
   });
 });
 
 //get everything after colon
 router.get('/:id', function(req, res, next){
   var id = req.params.id;
-  console.log('dude');
+  // console.log('dude');
   model.getById(id, function(err, particle) {
     console.log(err);
     if (err) return next(err);
