abbe80503d19f15e44a449c041f3b07ee0c4b034
Mudit Ameta <zeusdeux@gmail.com>
2015-06-24 22:30:53 -0400

    Changed show path from /:id to /show/:id to fix the id = 'save' issue. Also fixed the redirect to error on invalid id

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,23 @@ var router = express.Router();
 var model = require('../model');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'PARTICLES', name: "Samantha" });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'PARTICLES', name: 'Samantha' });
 });
 
-router.get('/save', function(req, res, next){
-  var idnum = model.save(req.query.msg, function(err, particle) {
+router.get('/save', function(req, res, next) {
+  model.save(req.query.msg, function(err, particle) {
     if (err) return next(err);
-    // res.render('create', {id: particle._id});
-
     res.render('create', {data: particle});
   });
 });
 
-//get everything after colon
-router.get('/:id', function(req, res, next){
+// get everything after colon
+router.get('/show/:id', function(req, res, next) {
   var id = req.params.id;
-  // console.log('dude');
+
   model.getById(id, function(err, particle) {
-    console.log(err);
     if (err) return next(err);
-    console.log(particle);
     res.render('particles', {title: 'PARTICLES', data: particle});
   });
 });
